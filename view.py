5a695edfd0840e149a7a44803a9c69c62f7886d2
Keefe Aldwin Magdato <keefemagdato@gmail.com>
2025-03-05 13:47:41 +0800

    update

--- view.py
+++ view.py
@@ -1,2 +1,32 @@
+import pyxel
+
+from project_types import UpdateHandler
+
+
 class View:
-	...
+	def start(self, screen_width:int, screen_height:int, fps: int):
+		pyxel.init(screen_width, screen_height, fps= fps) # you can add fps for third parameter
+		pyxel.run(self.update, self.draw) 
+
+
+	def update(self):
+		...
+
+	def draw(self):
+		...
+
+
+
+	def temp(self):
+		pyxel.cls(0)
+
+		for pipe_pair in pipes:
+			for pipe in [pipe_pair.top_pipe, pipe_pair.bottom_pipe]:
+				pyxel.rect(pipe.x, pipe.y, pipe.width, pipe.height, 5)
+
+		pyxel.circ(bird.x, bird.y, bird.radius, 7)
+
+		pyxel.text(SCREEN_WIDTH // 2 - 3, 15, str(score), 8)
+
+		if is_game_over:
+			pyxel.text(SCREEN_WIDTH // 2 - 15, SCREEN_HEIGHT // 2, "GAME OVER", 8)
